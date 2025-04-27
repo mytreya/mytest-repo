342cfbec263e4560f16c5c5e142426dfb37743c1
mytreya <mytreya@gmail.com>
2025-04-27 19:51:13 +0530

    modify hello msg

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Tekton CI/CD pipeline!"
+    return "Hi from Tekton CI/CD pipeline!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
